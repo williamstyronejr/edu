f87230c57d729b781a0d70bda6eea4045715bed1
williamstyronejr <itenory@gmail.com>
2021-03-25 11:35:19 -0400

    Initial commit

---
+++ src/components/exercises/boards/example.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Game from '../../shared/Game';
+
+const Tutorial = (props: Object) => {
+  return <Game>{/* Put game objects here to create a board */}</Game>;
+};
+
+export default Tutorial;
