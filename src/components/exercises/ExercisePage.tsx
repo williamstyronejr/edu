6a6052b0327db1c10f0107b320c99b936826a8e2
williamstyronejr <itenory@gmail.com>
2021-03-25 11:36:07 -0400

    Initial commit

---
+++ src/components/exercises/ExercisePage.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { RouteComponentProps } from 'react-router-dom';
+import './styles/exercisePage.css';
+
+type TParams = { id: string };
+
+const NotFound = () => {
+  return (
+    <section className="exercise exercise--missing">
+      <div className="exercise__message">404</div>
+    </section>
+  );
+};
+
+function ErrorFallback() {
+  return (
+    <div className="">
+      <p>This exercise could not be found. </p>
+    </div>
+  );
+}
+
+const ExercisePage = ({ match }: RouteComponentProps<TParams>) => {
+  const { id } = match.params;
+
+  if (!id) return <NotFound />;
+  const CurrentComp = React.lazy(() => import(`./boards/000-tutorial`));
+
+  return (
+    <section className="exercise">
+      <React.Suspense fallback={<div>loadding....</div>}>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <CurrentComp />
+        </ErrorBoundary>
+      </React.Suspense>
+    </section>
+  );
+};
+
+export default ExercisePage;
---
+++ src/components/exercises/boards/000-tutorial.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Game from '../../shared/Game';
+import Platform from '../../shared/Platform';
+
+const Tutorial = (props: Object) => {
+  return (
+    <Game>
+      <Platform />
+    </Game>
+  );
+};
+
+export default Tutorial;
